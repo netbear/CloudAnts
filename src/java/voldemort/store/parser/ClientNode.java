1701e5edb1c20d9662a5a9514e475187b6173b9d
Ringo Guo <ringoguo@gmail.com>
2011-06-23 16:52:16 +0800

    Fix bugs when transfering ClientNode bytes

--- src/java/voldemort/store/parser/ClientNode.java
+++ src/java/voldemort/store/parser/ClientNode.java
@@ -21,11 +21,14 @@ public class ClientNode {
     }
 
     public ClientNode(byte[] bytes, int offset) {
-        int length = bytes[offset + 1];
+        int length = bytes[offset];
         byte[] clientBytes = ByteUtils.copy(bytes, offset + 1, offset + length + 1);
         String c = ByteUtils.getString(clientBytes, "UTF-8");
         this.client = c;
-        this.version = new VectorClock(bytes, offset + 1 + length);
+        if(bytes[offset + length + 1] == 1)
+            this.version = new VectorClock(bytes, offset + 2 + length);
+        else
+            this.version = null;
 
     }
 
@@ -38,14 +41,16 @@ public class ClientNode {
     }
 
     public int sizeInBytes() {
-        return client.length() + 1 + version.sizeInBytes();
+        return client.length() + 2 + (version == null ? 0 : version.sizeInBytes());
     }
 
     public byte[] toBytes() {
-        byte[] clientBytes = ByteUtils.getBytes(client, "UTF=8");
+        byte[] clientBytes = ByteUtils.getBytes(client, "UTF-8");
+
         byte[] serialized = ByteUtils.cat(new byte[] { (byte) client.length() },
                                           clientBytes,
-                                          version.toBytes());
+                                          new byte[] { (byte) (version == null ? 0 : 1) },
+                                          version == null ? new byte[0] : version.toBytes());
         return serialized;
     }
 
@@ -59,7 +64,8 @@ public class ClientNode {
         if(version == null) {
             version = v;
         } else {
-            System.out.println("current version :" + version + " new version : " + v);
+            // System.out.println("current version :" + version +
+            // " new version : " + v);
             List<ClockEntry> versions = v.getEntries();
             List<ClockEntry> clientVersions = version.getEntries();
             List<ClockEntry> newVersions = new ArrayList<ClockEntry>();
